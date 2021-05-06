f0712a245bc76ffcd5a56b6a7f190421b1a68067
ProLyfe <rayane.prolyfe@gmail.com>
2021-05-06 23:16:03 +0200

    [add] capitales quizz json

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ const io = socket(server)
 let users = [];
 
 io.on('connection', socket => {
-    socket.emit('connecter', 'Vous êtes connecté');
+    socket.emit('connecter');
     socket.on('newUser', data => {
-        // console.log(data)
         users.push(data);
         socket.broadcast.emit('sendUsersList', users);
     });
-
 });
 
-server.listen(8900, () => {
+server.listen(8800, () => {
     console.log('Connecté au serveur');
 });
